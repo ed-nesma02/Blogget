65b3ccfb41b66915c02dc214dd84dce565b191d5
ed-nesma02 <shkurko26081978@gmail.com>
2023-09-14 23:39:45 +0300

    lesson 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Header} from './components/Header/Header';
 import {Main} from './components/Main/Main';
+import {useToken} from './hooks/useToken';
 
 function App() {
+  const [token, delToken] = useToken('');
+
   return (
     <React.Fragment>
-      <Header />
+      <Header token={token} delToken={delToken} />
       <Main />
     </React.Fragment>
   );
